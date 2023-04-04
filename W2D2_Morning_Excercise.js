e06ce3985fe4e424c8f7f1df6aa5534c05e555c6
Aldrich Lapid <alapid@LABA.PSWAS.LOCAL>
2023-04-04 09:57:19 -0700

    finished W2D2 Morning Exercise

---
+++ W2D2_Morning_Excercise.js
@@ -0,0 +1,27 @@
+console.log (Hello)
+
+const calculateSquareArea = (width, height) => {
+    return width * height 
+}
+
+calculateSquareArea(25, 25)
+
+let convertFahrenheitToCelcius = () => {
+    return (farenheit -32) * 5 / 9;
+}
+
+convertFahrenheitToCelcius(90);
+
+var isEven = (number) => { 
+    if(number % 2 === 0) {
+    console.log (number_is_even)
+    }
+}
+
+isEven(6);
+
+let concatenateStrings = (str1, str2) => {
+    return str1 + str2;
+}
+
+concatenateStrings("Hello, my name is", " Aldrich")
